303313a4cc9f6a8da15605eaf9290032f8e33b00
Doug Chivers <doug.chivers@hp.com>
2015-08-12 14:23:27 +0100

    Added status indicator, tested/hacked for windows

--- jpgcheck.py
+++ jpgcheck.py
@@ -1,10 +1,16 @@
 from PIL import Image
 import os
 
+#counters
 failed = 0
 small = 0
-sizex = 3200
-sizey = 2000
+good = 0
+
+# dimensions to define 'small' images
+sizex = 320
+sizey = 200
+
+#directories
 smalldir = "small"
 gooddir = "good"
 brokendir = "broken"
@@ -20,4 +26,13 @@ for file in os.listdir("."):
             os.rename(file,"./" + gooddir + "/" + file)
         except:
           failed += 1
-          os.rename(file,"./" + brokendir + "/" + file)
+	  # On windows this throws an exception due to the file still being open from above,
+          # too lazy to fix so just leaving broken files where they are for the moment 
+          #os.rename(file,"./" + brokendir + "/" + file)
+
+    if (failed + good + small) % 1000 == 0:
+        print "Processed %d broken: %d small: %d good: %d" % ((failed + good + small), failed, small, good)
+
+
+print "\nFinished\n------------\n Total: %d broken: %d small: %d good: %d" % ((failed + good + small), failed, small, good)
+
